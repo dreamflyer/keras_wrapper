141ab5b3fe5ca72beb97d27ee10492ddcd33a2e7
dreamflyer <gleb@onpositive.com>
2020-01-27 18:27:05 +0700

    some updates

--- keras/utils/__init__.py
+++ keras/utils/__init__.py
@@ -8,7 +8,7 @@ from tensorflow.python.keras.utils.generic_utils import Progbar
 from tensorflow.python.keras.utils.generic_utils import custom_object_scope
 from tensorflow.python.keras.utils.generic_utils import deserialize_keras_object
 from tensorflow.python.keras.utils.generic_utils import get_custom_objects
-from tensorflow.python.keras.utils.generic_utils import register_keras_serializable
+#from tensorflow.python.keras.utils.generic_utils import register_keras_serializable
 from tensorflow.python.keras.utils.generic_utils import serialize_keras_object
 from tensorflow.python.keras.utils.io_utils import HDF5Matrix
 from tensorflow.python.keras.utils.layer_utils import convert_all_kernels_in_model
--- keras/utils/generic_utils.py
+++ keras/utils/generic_utils.py
@@ -3,5 +3,5 @@ from tensorflow.python.keras.utils.generic_utils import Progbar
 from tensorflow.python.keras.utils.generic_utils import custom_object_scope
 from tensorflow.python.keras.utils.generic_utils import deserialize_keras_object
 from tensorflow.python.keras.utils.generic_utils import get_custom_objects
-from tensorflow.python.keras.utils.generic_utils import register_keras_serializable
+#from tensorflow.python.keras.utils.generic_utils import register_keras_serializable
 from tensorflow.python.keras.utils.generic_utils import serialize_keras_object
